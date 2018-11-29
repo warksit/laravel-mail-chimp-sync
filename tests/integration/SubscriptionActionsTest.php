29799cf372fda7f2a14df63725463322d4cc004c
Andrew Westrope <andrew@warwickshire.it>
2018-11-29 15:07:28 +1000

    move duplicate test

--- tests/integration/SubscriptionActionsTest.php
+++ tests/integration/SubscriptionActionsTest.php
@@ -66,18 +66,6 @@ class SubscriptionActionsTest extends TestCase
 
     /** @test
      * @group  */
-    public function check()
-    {
-        $sa = new SubscriptionActions(new Client());
-
-        $mc = $sa->subscribe($this->model);
-
-        $this->assertEquals('d7858eaa2088ffd13a2c5e687ee68437',$mc->id);
-        $this->assertEquals('0a12b6a14e',$mc->unique_email_id);
-    }
-
-    /** @test
-     * @group  */
     public function it_un_subscribes_from_mailChimp()
     {
         $sa = new SubscriptionActions(new Client());
